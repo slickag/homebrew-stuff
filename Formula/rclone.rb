bc43c245221bd1cb2984045bb860bba4bac5cb1c
John Faust <45057319+slickag@users.noreply.github.com>
2025-10-16 14:03:43 +0000

    rclone 1.71.1 (new formula)
    Closes #3.
    
    Signed-off-by: John Faust <45057319+slickag@users.noreply.github.com>

---
+++ Formula/rclone.rb
@@ -0,0 +1,36 @@
+class Rclone < Formula
+  desc "Rsync for cloud storage"
+  homepage "https://rclone.org/"
+  url "https://github.com/rclone/rclone/archive/refs/tags/v1.71.1.tar.gz"
+  sha256 "a3aa14e37047081f9770d7c58a0f13e665ed99600259884246b1884fc4b30b6c"
+  license "MIT"
+  head "https://github.com/rclone/rclone.git", branch: "master"
+
+  depends_on "go" => :build
+
+  on_linux do
+    depends_on "libfuse@2"
+  end
+
+  def install
+    ENV["GOPATH"] = prefix.to_s
+    ENV["GOBIN"] = bin.to_s
+    ENV["GOMODCACHE"] = "#{HOMEBREW_CACHE}/go_mod_cache/pkg/mod"
+    ENV["CGO_FLAGS"] = "-g -O3"
+    args = ["GOTAGS=cmount"]
+    system "make", *args
+    man1.install "rclone.1"
+    system bin/"rclone", "genautocomplete", "bash", "rclone.bash"
+    system bin/"rclone", "genautocomplete", "zsh", "_rclone"
+    system bin/"rclone", "genautocomplete", "fish", "rclone.fish"
+    bash_completion.install "rclone.bash" => "rclone"
+    zsh_completion.install "_rclone"
+    fish_completion.install "rclone.fish"
+  end
+
+  test do
+    (testpath/"file1.txt").write "Test!"
+    system bin/"rclone", "copy", testpath/"file1.txt", testpath/"dist"
+    assert_match File.read(testpath/"file1.txt"), File.read(testpath/"dist/file1.txt")
+  end
+end
